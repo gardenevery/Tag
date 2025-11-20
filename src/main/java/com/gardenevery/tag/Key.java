cc2c0e9147ab798445c8d64b5ff14cba57bc9ff2
gardenevery <dddccc123@163.com>
2025-11-20 21:36:04 +0800

    修改key

--- src/main/java/com/gardenevery/tag/Key.java
+++ src/main/java/com/gardenevery/tag/Key.java
@@ -1,6 +1,5 @@
 package com.gardenevery.tag;
 
-import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -16,118 +15,118 @@ import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 interface Key {
-//    @Desugar
-//    record ItemKey(@Nonnull Item item, int metadata) implements Key {
-//        @Nullable
-//        public static ItemKey from(@Nullable ItemStack stack) {
-//            if (stack == null || stack.isEmpty()) {
-//                return null;
-//            }
-//            int metadata = stack.getHasSubtypes() ? stack.getMetadata() : 0;
-//            return new ItemKey(stack.getItem(), metadata);
-//        }
-//
-//        @Nonnull
-//        public ItemStack stack() {
-//            return new ItemStack(item, 1, metadata);
-//        }
-//    }
-//
-//    @Desugar
-//    record FluidKey(@Nonnull Fluid fluid) implements Key {
-//        @Nullable
-//        public static FluidKey from(@Nullable FluidStack stack) {
-//            if (stack == null || stack.getFluid() == null) {
-//                return null;
-//            }
-//            return new FluidKey(stack.getFluid());
-//        }
-//
-//        @Nonnull
-//        public FluidStack stack() {
-//            return new FluidStack(fluid, 1000);
-//        }
-//    }
-//
-//    @Desugar
-//    record BlockKey(@Nonnull Block block) implements Key {
-//        @Nullable
-//        public static BlockKey from(@Nullable Block block) {
-//            if (block == null) {
-//                return null;
-//            }
-//            return new BlockKey(block);
-//        }
-//
-//        public Block toBlock() {
-//            return block;
-//        }
-//    }
-
     @Desugar
-    record ItemKey(@Nonnull ResourceLocation item, int metadata) implements Key {
+    record ItemKey(@Nonnull Item item, int metadata) implements Key {
         @Nullable
         public static ItemKey from(@Nullable ItemStack stack) {
             if (stack == null || stack.isEmpty()) {
                 return null;
             }
-
-            var item = stack.getItem();
-            var registryName = item.getRegistryName();
-            if (registryName == null) {
-                return null;
-            }
             int metadata = stack.getHasSubtypes() ? stack.getMetadata() : 0;
-            return new ItemKey(registryName, metadata);
+            return new ItemKey(stack.getItem(), metadata);
         }
 
+        @Nonnull
         public ItemStack stack() {
-            return new ItemStack(Objects.requireNonNull(ForgeRegistries.ITEMS.getValue(item)), 1, metadata);
+            return new ItemStack(item, 1, metadata);
         }
     }
 
     @Desugar
-    record FluidKey(@Nonnull String fluid) implements Key {
+    record FluidKey(@Nonnull Fluid fluid) implements Key {
         @Nullable
         public static FluidKey from(@Nullable FluidStack stack) {
             if (stack == null || stack.getFluid() == null) {
                 return null;
             }
-
-            var fluidName = FluidRegistry.getFluidName(stack.getFluid());
-            if (fluidName == null) {
-                return null;
-            }
-            return new FluidKey(fluidName);
+            return new FluidKey(stack.getFluid());
         }
 
+        @Nonnull
         public FluidStack stack() {
-            var fluidName = fluid();
-            var fluid = FluidRegistry.getFluid(fluidName);
-            if (fluid == null) {
-                return null;
-            }
             return new FluidStack(fluid, 1000);
         }
     }
 
     @Desugar
-    record BlockKey(@Nonnull ResourceLocation block) implements Key {
+    record BlockKey(@Nonnull Block block) implements Key {
         @Nullable
         public static BlockKey from(@Nullable Block block) {
             if (block == null) {
                 return null;
             }
-
-            var registryName = block.getRegistryName();
-            if (registryName == null) {
-                return null;
-            }
-            return new BlockKey(registryName);
+            return new BlockKey(block);
         }
 
         public Block toBlock() {
-            return ForgeRegistries.BLOCKS.getValue(this.block);
+            return block;
         }
     }
+
+//    @Desugar
+//    record ItemKey(@Nonnull ResourceLocation item, int metadata) implements Key {
+//        @Nullable
+//        public static ItemKey from(@Nullable ItemStack stack) {
+//            if (stack == null || stack.isEmpty()) {
+//                return null;
+//            }
+//
+//            var item = stack.getItem();
+//            var registryName = item.getRegistryName();
+//            if (registryName == null) {
+//                return null;
+//            }
+//            int metadata = stack.getHasSubtypes() ? stack.getMetadata() : 0;
+//            return new ItemKey(registryName, metadata);
+//        }
+//
+//        public ItemStack stack() {
+//            return new ItemStack(ForgeRegistries.ITEMS.getValue(item), 1, metadata);
+//        }
+//    }
+//
+//    @Desugar
+//    record FluidKey(@Nonnull String fluid) implements Key {
+//        @Nullable
+//        public static FluidKey from(@Nullable FluidStack stack) {
+//            if (stack == null || stack.getFluid() == null) {
+//                return null;
+//            }
+//
+//            var fluidName = FluidRegistry.getFluidName(stack.getFluid());
+//            if (fluidName == null) {
+//                return null;
+//            }
+//            return new FluidKey(fluidName);
+//        }
+//
+//        public FluidStack stack() {
+//            var fluidName = fluid();
+//            var fluid = FluidRegistry.getFluid(fluidName);
+//            if (fluid == null) {
+//                return null;
+//            }
+//            return new FluidStack(fluid, 1000);
+//        }
+//    }
+//
+//    @Desugar
+//    record BlockKey(@Nonnull ResourceLocation block) implements Key {
+//        @Nullable
+//        public static BlockKey from(@Nullable Block block) {
+//            if (block == null) {
+//                return null;
+//            }
+//
+//            var registryName = block.getRegistryName();
+//            if (registryName == null) {
+//                return null;
+//            }
+//            return new BlockKey(registryName);
+//        }
+//
+//        public Block toBlock() {
+//            return ForgeRegistries.BLOCKS.getValue(this.block);
+//        }
+//    }
 }
